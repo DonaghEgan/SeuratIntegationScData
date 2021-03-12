2b9efed8d4003e869e34648e3d5f45f0d260b2b5
DonaghEgan <donagh.egan@ucdconnect.ie>
2021-03-12 19:37:41 +0000

    data_setup file

--- R/data_setup.R
+++ R/data_setup.R
@@ -1,6 +1,8 @@
 # Library
 library(readr)
 
-
 # Read in BCC metadata from TISH et al.,
 bcc_meta <- read_tsv("/home/degan/SeuratIntegationScData/TISCH_BCC/BCC_GSE123813_aPD1_CellMetainfo_table.tsv")
+
+# read in seurat object from integration analysis
+
