d4735656d60c09348382cedbcd2a9707284d1b61
DonaghEgan <donagh.egan@ucdconnect.ie>
2021-03-12 19:21:51 +0000

    Bash file and urls used for downloading

---
+++ Bash/TISCH_data_download.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+###### download following BCC items from TISCH website:
+###### single-cell expression matrix, cluster-averaged expression matrix
+###### DE gene table, Meta-information 
+
+wget -i TISCH_download_urls
+
+mkdir TISCH_BCC
+mkdir TISCH_mel
+
+
+
+
+
