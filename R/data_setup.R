9a2ea2c6c66b8266a5ee75eb412331ee933da5bc
DonaghEgan <donagh.egan@ucdconnect.ie>
2021-03-12 20:32:33 +0000

    Rscripts

--- R/data_setup.R
+++ R/data_setup.R
@@ -1,8 +1,19 @@
 # Library
 library(readr)
+library(Seurat)
 
 # Read in BCC metadata from TISH et al.,
 bcc_meta <- read_tsv("/home/degan/SeuratIntegationScData/TISCH_BCC/BCC_GSE123813_aPD1_CellMetainfo_table.tsv")
 
 # read in seurat object from integration analysis
+immune_combined <- readRDS("/home/degan/integration_2.0_march/saved_objects/immune_combined.Rds")
+
+# providing cells with annotations 
+immune_combined <- RenameIdents(immune_combined, `0` = "Naive CD4 T cells", `1` = "Chronically act/ex CD8 T cells", `2` = "CD8 Memory T cells", 
+                                `3` = "B cells_1", `4` = "CD8 active T cells", `5` = "Monocytes/Macrophages", `6` = "CD8 effector memory T cells", 
+                                `7` = "Tregs", `8` = "Plasma Cells", 
+                                `9` = "early activated T cells", `10` = "Tumor_1", `11` = "early activated CD8 T cells", 
+                                `12` = "Proliferative T cells", 
+                                `13` = "Fibroblasts/Melanocytes", `14` = "CD8 exhausted T cells", `15` = "pDCs", `16` = "Tumor_2", `17`= "NK cells" , 
+                                `18` = "Mature DCs",`19` = "Endothelial cells", `20` = "B cells_2", `21` = "Intermediate monocyte")
 
